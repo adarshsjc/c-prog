0829aa8901384b455ebdcd8deb54de9e1fe2bcb9
adarshsjc <av200489adarshvish.vg@gmail.com>
2025-03-25 16:17:56 +0530

    Add files via upload

---
+++ fac.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int fact(int num)
+{
+  int i,f=1;
+  for (i= num; i>=1;i--)
+{
+  f= f*i;
+}
+return f;
+}
+int main ()
+{
+int num , custom;
+printf("Enter a number:");
+scanf("%d",&num);
+custom =fact(num);
+
+printf("The factorial of %d is : %d\n",num ,custom);
+return 0;
+}  
